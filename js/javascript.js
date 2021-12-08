9f91b89ed4e134a3c3390eb944e103d362582f57
paulohenriquest <paulohenriquest@hotmail.com>
2021-12-07 21:44:54 -0300

    commit exercicios

---
+++ js/javascript.js
@@ -0,0 +1,107 @@
+
+// for (numero = 0; numero <= 5; numero++){
+//         console.log(numero);
+//     }
+//     let resultado = document.getElementById('tabuada');
+//     for(let numero = 1; numero <= 5; numero=numero+1){
+//         for(let multiplica = 0; multiplica <= 5; multiplica=multiplica+1){
+//             let multiplicacao = numero * multiplica;
+//             let linha = "" + numero + " * " + multiplica + " = " + multiplicacao + "<br>";
+//             resultado.innerHTML += (linha);
+//         }
+//     }
+
+// function perguntar(){
+//     let aviso = parseInt(prompt(' Tabuada de 0 a 2 \n \n 1 - Tabuada de 0 a 4 \n 2 - Tabuada de 0 a 6 \n 3 - Tabuada de 0 a 8 \n 4 - Tabuada de 0 a 10. '));
+//     let resultado = document.getElementById('tabuada');
+    
+//     switch(aviso){
+//         case 0:
+//             for (numero = 0; numero <= 2; numero++){
+//                         console.log(numero);
+//                     }
+//                     for(let numero = 1; numero <= 2; numero=numero+1){
+//                         for(let multiplica = 0; multiplica <= 2; multiplica=multiplica+1){
+//                             let multiplicacao = numero * multiplica;
+//                             let linha = "" + numero + " * " + multiplica + " = " + multiplicacao + "<br>";
+//                             resultado.innerHTML += (linha);
+//                         }
+//                     }
+//         break;
+
+//         case 1:
+//         for (numero = 0; numero <= 4; numero++){
+//             console.log(numero);
+//         }
+//         for(let numero = 1; numero <= 4; numero=numero+1){
+//             for(let multiplica = 0; multiplica <= 4; multiplica=multiplica+1){
+//                 let multiplicacao = numero * multiplica;
+//                 let linha = "" + numero + " * " + multiplica + " = " + multiplicacao + "<br>";
+//                 resultado.innerHTML += (linha);
+//             }
+//         }
+//         break;
+
+//         case 2:
+//         for (numero = 0; numero <= 6; numero++){
+//             console.log(numero);
+//         }
+//         for(let numero = 1; numero <= 6; numero=numero+1){
+//             for(let multiplica = 0; multiplica <= 6; multiplica=multiplica+1){
+//                 let multiplicacao = numero * multiplica;
+//                 let linha = "" + numero + " * " + multiplica + " = " + multiplicacao + "<br>";
+//                 resultado.innerHTML += (linha);
+//             }
+//         }
+//         break;
+
+//         case 3:
+//         for (numero = 0; numero <= 8; numero++){
+//             console.log(numero);
+//         }
+//         for(let numero = 1; numero <= 8; numero=numero+1){
+//             for(let multiplica = 0; multiplica <= 8; multiplica=multiplica+1){
+//                 let multiplicacao = numero * multiplica;
+//                 let linha = "" + numero + " * " + multiplica + " = " + multiplicacao + "<br>";
+//                 resultado.innerHTML += (linha);
+//             }
+//         }
+//         break;
+
+//         case 4:
+//         for (numero = 0; numero <= 8; numero++){
+//             console.log(numero);
+//         }
+//         for(let numero = 1; numero <= 8; numero=numero+1){
+//             for(let multiplica = 0; multiplica <= 8; multiplica=multiplica+1){
+//                 let multiplicacao = numero * multiplica;
+//                 let linha = "" + numero + " * " + multiplica + " = " + multiplicacao + "<br>";
+//                 resultado.innerHTML += (linha);
+//             }
+//         }
+//         break;
+
+//         case 5:
+//         for (numero = 0; numero <= 10; numero++){
+//             console.log(numero);
+//         }
+//         for(let numero = 1; numero <= 10; numero=numero+1){
+//             for(let multiplica = 0; multiplica <= 10; multiplica=multiplica+1){
+//                 let multiplicacao = numero * multiplica;
+//                 let linha = "" + numero + " * " + multiplica + " = " + multiplicacao + "<br>";
+//                 resultado.innerHTML += (linha);
+//             }
+//         }
+//         break;
+
+// function sexo(){
+//     let aviso = parseInt(document.getElementById('resultado'));
+//     if (aviso  ==  "m"){
+//         alert (fundo.style.background = 'blue');
+//     }else if(aviso  ==  "f"){
+//         alert (fundo.style.background = 'pink');
+//     }else{
+//     fundo.innerHTML = ('inválido')
+//     }
+// }
+
